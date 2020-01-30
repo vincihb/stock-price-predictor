fa0756b62117cd45fc1b75752b9fe385c85551ff
Michael Albinson <mikealbinson@gmail.com>
2020-01-29 23:06:17 -0500

    NLU Report for improved ticker accuracy as well as sorted post link scores

---
+++ experiments/michael/nlu_report.py
@@ -0,0 +1,108 @@
+import re
+
+from api.local_stocks.Ticker import Ticker
+from api.reddit.RedditAPI import RedditAPI
+from nlu.NLTKUtil import NLTKUtil
+from client.Reporter import Reporter
+from client.util.ChartBuilder import ChartBuilder
+from client.util.charts.DataSet import DataSet
+from client.util.html.TableBuilder import TableBuilder
+from client.util.html.LinkBuider import LinkBuilder
+from client.util.html.ScrollableDiv import ScrollableDiv
+
+# gather the current top 50 posts from WSB
+reddit = RedditAPI()
+submissions = reddit.get_hot('wallstreetbets', limit=1000)
+
+# loop through and try to pick out tickers
+tickers_found = {}
+for sub in range(0, len(submissions['title'])):
+    title = submissions['title'][sub]
+    self_text = submissions['body'][sub]
+    url = submissions['url'][sub]
+    score = submissions['score'][sub]
+
+    ticker = None
+    sorted_bow = NLTKUtil.get_weighted_stock_count(title, self_text)
+    for word in sorted_bow:
+        match = re.search('[A-Z]{2,4}', word)
+        if match is not None:
+            match_result = match.group(0).strip()
+            t = Ticker()
+            result = t.get_ticker(match_result.upper())
+            if result is not None:
+                ticker = result
+                break
+
+    if ticker is None:
+        continue
+
+    ticker_symbol = ticker[0]
+
+    # if it is, write down information about the ticker and how many times we've seen it
+    if ticker_symbol in tickers_found:
+        tickers_found[ticker_symbol]['count'] += 1
+        tickers_found[ticker_symbol]['submissions'].append({'link': url, 'title': title, 'score': score})
+    else:
+        tickers_found[ticker_symbol] = {
+            'count': 1,
+            'submissions': [{'link': url, 'title': title, 'score': score}],
+            'name': LinkBuilder(ticker[1], ticker[-3]),
+            'description': ticker[2]
+        }
+
+
+def sort_dict_by_score(item):
+    return item['score']
+
+
+def sort_by_mentions(item):
+    return item[1]
+
+
+# sort the submissions by score
+for tf in tickers_found:
+    tickers_found[tf]['submissions'].sort(reverse=True, key=sort_dict_by_score)
+
+# then reformat the result so that we can put it in a tabular format
+table_values = []
+for tf in tickers_found:
+    addendum = ''
+    counter = 0
+    for submission in tickers_found[tf]['submissions']:
+        addendum += LinkBuilder('[%d] - %d' % (counter, submission['score']), submission['link']).compile() + '<br />'
+        counter += 1
+
+    addendum = ScrollableDiv(addendum, '5rem').compile()
+
+    desc = '...'
+    if 'description' in tickers_found[tf] and tickers_found[tf]['description'] is not None:
+        desc = tickers_found[tf]['description']
+
+    table_values.append([tf, tickers_found[tf]['count'], tickers_found[tf]['name'],
+                         desc[:200] + '...', addendum])
+
+table_values.sort(key=sort_by_mentions, reverse=True)
+
+# and then mutate the data again to match the data format for bar charts
+x = []
+y = []
+for arr in table_values:
+    x.append(arr[0])
+    y.append(arr[1])
+
+ds = DataSet()
+ds.set_x(x)
+ds.append_y_set({'data': y, 'label': ''})
+
+# Build the actual report from our parsed data
+report = Reporter()
+report.set_title('NLU Report')
+
+# set up a table
+table_header = ['Ticker', 'Mentions', 'Name', 'Description', 'Links']
+report.set_body(TableBuilder(headers=table_header, rows=table_values))
+
+# and a chart
+report.append_to_body(ChartBuilder(title='WSB Mentions', chart_type='bar', data_set=ds))
+report.compile()
--- nlu/NLTKUtil.py
+++ nlu/NLTKUtil.py
@@ -8,9 +8,12 @@ class NLTKUtil:
     def get_bag_of_words(corpus):
         stop_words = list(punctuation) + stopwords.words('english')
         bow = []
-        for w in corpus.lower().split():
-            # don't carry single letters into the BOW
-            if w not in stop_words and len(w) > 1:
+        for w in corpus.split():
+            # if the last character is punctuation, split it off
+            if w[len(w) - 1] in punctuation:
+                w = w[:-1]
+
+            if w not in stop_words:
                 bow.append(w)
 
         return bow
@@ -22,3 +25,29 @@ class NLTKUtil:
             return dict(Counter(bow))
         else:
             return Counter(bow)
+
+    @staticmethod
+    def get_weighted_count(title, body, weight=5):
+        title_bow = NLTKUtil.get_counted_bag_of_words(title)
+        body_bow = NLTKUtil.get_counted_bag_of_words(body)
+        for key in title_bow:
+            if key in body_bow:
+                body_bow[key] += weight * title_bow[key]
+            else:
+                body_bow[key] = weight * title_bow[key]
+
+        body_bow = NLTKUtil.sort_bow(body_bow)
+        return body_bow
+
+    @staticmethod
+    def get_weighted_stock_count(title, body, weight=5, suspect_ticker_weight=2):
+        bow = NLTKUtil.get_weighted_count(title, body, weight)
+        for key in bow:
+            if 1 < len(key) < 5:
+                bow[key] = bow[key] * suspect_ticker_weight
+
+        return bow
+
+    @staticmethod
+    def sort_bow(dictionary):
+        return {k: v for k, v in sorted(dictionary.items(), key=lambda item: item[1], reverse=True)}
