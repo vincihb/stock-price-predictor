9d3cac180d83a1d919031aa9091fcc07fee77772
Michael Albinson <mikealbinson@gmail.com>
2020-02-01 14:08:34 -0500

    Secondary attempt to improve match

--- nlu/NLTKUtil.py
+++ nlu/NLTKUtil.py
@@ -68,9 +68,17 @@ class NLTKUtil:
     @staticmethod
     def get_likely_subject_stock(sorted_bow):
         for word in sorted_bow:
-            match = re.search('^[A-Z]{2,4}$', word)
-            if match is not None and not any(word in not_ticker for not_ticker in NOT_TICKER_LIST):
-                match_result = match.group(0).strip()
+            primary_search = re.search('^\$[A-Z]{1,5}')
+            if primary_search is not None and not any(word in not_ticker for not_ticker in NOT_TICKER_LIST):
+                match_result = primary_search.group(0).strip()
+                t = Ticker()
+                result = t.get_ticker(match_result.upper())
+                if result is not None:
+                    return result
+
+            secondary_search = re.search('^[A-Z]{2,4}$', word)
+            if secondary_search is not None and not any(word in not_ticker for not_ticker in NOT_TICKER_LIST):
+                match_result = secondary_search.group(0).strip()
                 t = Ticker()
                 result = t.get_ticker(match_result.upper())
                 if result is not None:
