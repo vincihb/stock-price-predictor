408b9fb3d1e69090e42d30def3cc695db79d1b92
Michael Albinson <mikealbinson@gmail.com>
2020-02-08 17:04:47 -0500

    Minor improvements

--- client/js/spec/index.js
+++ client/js/spec/index.js
@@ -1,17 +1,14 @@
 (function() {
     window.addEventListener('load', () => {
-        setTimeout(() => {
-            console.log('load');
-            window.document.querySelectorAll('button.gen-report').forEach((el) => {
-                el.addEventListener('click', (e) => {
-                    const button = e.target;
-                    const reportType = button.getAttribute('data-type');
+        window.document.querySelectorAll('button.gen-report').forEach((el) => {
+            el.addEventListener('click', (e) => {
+                const button = e.target;
+                const reportType = button.getAttribute('data-type');
 
-                    request('/report', { report_type: reportType }, (data) => {
-                        window.location.href = data.report_title
-                    }, console.error);
-                });
+                request('/report', { report_type: reportType }, (data) => {
+                    window.location.href = data.report_title
+                }, console.error);
             });
-        }, 200);
+        });
     });
 })();
