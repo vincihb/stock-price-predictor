6a1c0d32c899bd8023cd6692f180e9ac35e8fa5a
michael.albinson <michael.albinson@servicenow.com>
2020-02-18 13:03:46 -0500

    Clearing up some DD tools

--- api/reddit/wsb/DailyDiscussion.py
+++ api/reddit/wsb/DailyDiscussion.py
@@ -13,6 +13,7 @@ class DailyDiscussion:
         self._cache_path = path.join(self._local_dir, 'cache')
 
     def get_daily_discussion(self, for_date=None, strict_match=False):
+        date_result = None
         if for_date is not None and isinstance(for_date, date):
             for_date = for_date.toordinal()
 
@@ -40,6 +41,12 @@ class DailyDiscussion:
             post_meta = self._get_post_meta(post)
             self._dump_to_cache(post_meta, post_file_name)
 
+            # if a date is specified...  strict matching is enforced for scraping the data from reddit
+            if for_date is not None and date.fromtimestamp(post.created).toordinal() == for_date:
+                date_result = post_meta
+
+        return date_result
+
     def _get_post_meta(self, post):
         post_meta = {
             "title": post.title,
@@ -52,7 +59,7 @@ class DailyDiscussion:
             "misc": []
         }
 
-        comments = self.resolve_all_comments(post.comments)
+        comments = self._resolve_all_comments(post.comments)
         for comment in comments:
             if isinstance(comment, MoreComments):
                 continue
@@ -81,7 +88,7 @@ class DailyDiscussion:
             for_date = for_date.toordinal()
 
         found_dd = self._is_in_cache(for_date, strict_match=strict_match)
-        return [self.get_from_cache(dd) for dd in found_dd]
+        return [self._get_from_cache(dd) for dd in found_dd]
 
     def _is_in_cache(self, for_date, strict_match=False, _from_fuzz=False):
         if isinstance(for_date, date):
@@ -92,21 +99,21 @@ class DailyDiscussion:
         # return an array of the files where its timestamp matches the date you're searching for
         found = [dd for dd in files if date.fromtimestamp(int(dd.split('-')[0])).toordinal() == for_date]
         if len(found) == 0 and not _from_fuzz and not strict_match:
-            return self.fuzz_for_dd(for_date)
+            return self._fuzz_for_dd(for_date)
 
         return found
 
     # If we don't find data, recursively walking back in time to find some
-    def fuzz_for_dd(self, for_date, _current_depth=0, _max_depth=5):
+    def _fuzz_for_dd(self, for_date, _current_depth=0, _max_depth=5):
         last_date = for_date - 1
         dd = self._is_in_cache(last_date, _from_fuzz=True)
         if len(dd) == 0 and _current_depth < _max_depth:
-            return self.fuzz_for_dd(last_date, _current_depth + 1)
+            return self._fuzz_for_dd(last_date, _current_depth + 1)
 
         return dd
 
     @staticmethod
-    def resolve_all_comments(post_comments):
+    def _resolve_all_comments(post_comments):
         comments = list(post_comments)
         while isinstance(comments[-1], MoreComments):
             print('Getting more comments...')
@@ -125,9 +132,9 @@ class DailyDiscussion:
         }
 
     def _check_cache(self, post_id):
-        return self.get_from_cache(post_id + ".pickle")
+        return self._get_from_cache(post_id + ".pickle")
 
-    def get_from_cache(self, filename):
+    def _get_from_cache(self, filename):
         return Pickler.load_obj(path.join(self._cache_path, filename))
 
     def _dump_to_cache(self, obj, post_id):
