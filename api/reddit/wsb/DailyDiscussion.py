109ba588d3d1887d1733b8e3fed8fc1388db4e67
michael.albinson <michael.albinson@servicenow.com>
2020-02-18 12:45:12 -0500

    Add functionality to retrieve cache data for a date

--- api/reddit/wsb/DailyDiscussion.py
+++ api/reddit/wsb/DailyDiscussion.py
@@ -1,9 +1,9 @@
 from api.reddit.RedditAPI import RedditAPI
 from api.util.Pickler import Pickler
 from nlu.NLUSubjectTickerEstimator import NLUSubjectTickerEstimator
-from os import path
+from os import path, listdir
 from praw.models import MoreComments
-
+from datetime import date
 
 class DailyDiscussion:
     def __init__(self):
@@ -27,40 +27,73 @@ class DailyDiscussion:
                 continue
 
             # set up the collections
-            post_meta = {
-                "title": post.title,
-                "link": post.permalink,
-                "id": post.id,
-                'date': int(post.created)
-            }
-
-            tickers = {
-                "misc": []
-            }
-
-            comments = self.resolve_all_comments(post.comments)
-            for comment in comments:
-                if isinstance(comment, MoreComments):
-                    continue
-
-                body = comment.body
-
-                ticker = NLUSubjectTickerEstimator.estimate('', body)
-                print(body, ticker)
-                if ticker is None:
-                    tickers['misc'].append(self._serialize_comment(comment))
-                elif ticker in tickers:
-                    tickers[ticker]['count'] += 1
-                    tickers[ticker]['submissions'].append(self._serialize_comment(comment))
-                else:
-                    tickers[ticker] = {
-                        'count': 1,
-                        'submissions': [self._serialize_comment(comment)],
-                        'description': ticker[2]
-                    }
-
-            post_meta['tickers'] = tickers
+            post_meta = self.get_post_meta(post)
             self._dump_to_cache(post_meta, post_file_name)
+
+    def get_post_meta(self, post):
+        post_meta = {
+            "title": post.title,
+            "link": post.permalink,
+            "id": post.id,
+            'date': int(post.created)
+        }
+
+        tickers = {
+            "misc": []
+        }
+
+        comments = self.resolve_all_comments(post.comments)
+        for comment in comments:
+            if isinstance(comment, MoreComments):
+                continue
+
+            body = comment.body
+
+            ticker = NLUSubjectTickerEstimator.estimate('', body)
+            print(body, ticker)
+            if ticker is None:
+                tickers['misc'].append(self._serialize_comment(comment))
+            elif ticker in tickers:
+                tickers[ticker]['count'] += 1
+                tickers[ticker]['submissions'].append(self._serialize_comment(comment))
+            else:
+                tickers[ticker] = {
+                    'count': 1,
+                    'submissions': [self._serialize_comment(comment)],
+                    'description': ticker[2]
+                }
+
+        post_meta['tickers'] = tickers
+        return post_meta
+
+    def get_daily_discussion_meta(self, for_date):
+        if isinstance(for_date, date):
+            for_date = for_date.toordinal()
+
+        found_dd = self._is_in_cache(for_date)
+        return [self.get_from_cache(dd) for dd in found_dd]
+
+    def _is_in_cache(self, for_date, _from_fuzz=False):
+        if isinstance(for_date, date):
+            for_date = for_date.toordinal()
+
+        # read out the files in the cache
+        files = [f for f in listdir(self._cache_path) if path.isfile(path.join(self._cache_path, f)) and f != "README.md"]
+        # return an array of the files where its timestamp matches the date you're searching for
+        found = [dd for dd in files if date.fromtimestamp(int(dd.split('-')[0])).toordinal() == for_date]
+        if len(found) == 0 and not _from_fuzz:
+            return self.fuzz_for_dd(for_date)
+
+        return found
+
+    # If we don't find data, recursively walking back in time to find some
+    def fuzz_for_dd(self, for_date, _current_depth=0, _max_depth=5):
+        last_date = for_date - 1
+        dd = self._is_in_cache(last_date, _from_fuzz=True)
+        if len(dd) == 0 and _current_depth < _max_depth:
+            return self.fuzz_for_dd(last_date, _current_depth + 1)
+
+        return dd
 
     @staticmethod
     def resolve_all_comments(post_comments):
@@ -82,11 +115,11 @@ class DailyDiscussion:
         }
 
     def _check_cache(self, post_id):
-        return Pickler.load_obj(path.join(self._cache_path, post_id + ".pickle"))
+        return self.get_from_cache(post_id + ".pickle")
+
+    def get_from_cache(self, filename):
+        return Pickler.load_obj(path.join(self._cache_path, filename))
 
     def _dump_to_cache(self, obj, post_id):
         print('Dumping results to pickle file: ' + post_id + '.pickle')
         Pickler.save_obj(obj, path.join(self._cache_path, post_id + ".pickle"))
-
-
-DailyDiscussion().get_daily_discussion()
